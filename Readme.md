b599d502cde218563f6de33b703cc10c430a5678
astratow <davno_pora@mail.com>
2024-07-01 16:53:17 +0100

    readme update

--- Readme.md
+++ Readme.md
@@ -18,11 +18,5 @@
 ### Example Request
 
 ```bash
-curl -X POST http://localhost:8000/app/example/shift4 -H "Content-Type: application/json" -d '{
-    "amount": "100",
-    "currency": "USD",
-    "card_number": "4111111111111111",
-    "card_exp_year": "2025",
-    "card_exp_month": "12",
-    "card_cvv": "123"
-}'
+php bin/console app:example shift4 --amount=100 --currency=USD --card_number=4111111111111111 --card_exp_year=2025 --card_exp_month=12 --card_cvv=123
+
--- src/Service/PaymentGatewayService.php
+++ src/Service/PaymentGatewayService.php
@@ -47,47 +47,64 @@ class PaymentGatewayService
     {
         $this->logger->info('Processing Shift4 payment', ['data' => $paymentData]);
 
-        $username = $_ENV['SHIFT4_API_USERNAME'];
-        $password = $_ENV['SHIFT4_API_PASSWORD'];
+        // $username = $_ENV['SHIFT4_API_USERNAME'];
+        // $password = $_ENV['SHIFT4_API_PASSWORD'];
+        // <?php
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.shift4.com/charges');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+        ]);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, 'pr_test_tXHm9qV9qV9bjIRHcQr9PLPa:');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n    \"amount\": 500,\n    \"currency\": \"usd\",\n    \"card\": {\n      \"number\": \"4242424242424242\",\n      \"expMonth\": 12,\n      \"expYear\": 2024,\n      \"cvc\": \"123\"\n    },\n    \"description\": \"Example charge\"\n  }");
+        
+        $response = curl_exec($ch);
+        
+        curl_close($ch);
+        return[];
 
-        $requestPayload = [
-            'amount' => $paymentData['amount'],
-            'currency' => $paymentData['currency'],
-            'card' => [
-                'first6' => substr($paymentData['card_number'], 0, 6),
-                'last4' => substr($paymentData['card_number'], -4),
-                'expMonth' => $paymentData['card_exp_month'],
-                'expYear' => $paymentData['card_exp_year'],
-                'cvv' => $paymentData['card_cvv'],
-            ]
-        ];
+        // $requestPayload = [
+        //     'amount' => $paymentData['amount'],
+        //     'currency' => $paymentData['currency'],
+        //     'description' => $paymentData['description'],
+        //     'card' => [
+        //         'first6' => substr($paymentData['card_number'], 0, 6),
+        //         'last4' => substr($paymentData['card_number'], -4),
+        //         'expMonth' => $paymentData['card_exp_month'],
+        //         'expYear' => $paymentData['card_exp_year'],
+        //         'cvv' => $paymentData['card_cvv'],
+        //     ]
+        // ];
 
-        $this->logger->info('Shift4 request URL', ['url' => 'https://api.shift4.com/charges']);
-        $this->logger->info('Shift4 request payload', ['payload' => $requestPayload]);
+        // $this->logger->info('Shift4 request URL', ['url' => 'https://api.shift4.com/charges']);
+        // $this->logger->info('Shift4 request payload', ['payload' => $requestPayload]);
         
-        try {
-            $response = $this->client->request('POST', 'https://api.shift4.com/charges', [
-                'json' => $requestPayload,
-                'auth_basic' => [$username, $password ?: ''],
-            ]);
+        // try {
+        //     $response = $this->client->request('POST', 'https://api.shift4.com/charges', [
+        //         'json' => $requestPayload,
+        //         'auth_basic' => [$username, $password ?: ''],
+        //     ]);
         
-            $responseData = $response->toArray();
-            $this->logger->info('Shift4 response', ['response' => $responseData]);
+        //     $responseData = $response->toArray();
+        //     $this->logger->info('Shift4 response', ['response' => $responseData]);
         
-            return $responseData;
-        } catch (\Exception $e) {
-            if ($e instanceof \Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface) {
-                $response = $e->getResponse();
-                $responseContent = $response->getContent(false);
-                $this->logger->error('Error processing Shift4 payment', [
-                    'exception' => $e->getMessage(),
-                    'response' => $responseContent
-                ]);
-            } else {
-                $this->logger->error('Error processing Shift4 payment', ['exception' => $e->getMessage()]);
-            }
+        //     return $responseData;
+        // } catch (\Exception $e) {
+        //     if ($e instanceof \Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface) {
+        //         $response = $e->getResponse();
+        //         $responseContent = $response->getContent(false);
+        //         $this->logger->error('Error processing Shift4 payment', [
+        //             'exception' => $e->getMessage(),
+        //             'response' => $responseContent
+        //         ]);
+        //     } else {
+        //         $this->logger->error('Error processing Shift4 payment', ['exception' => $e->getMessage()]);
+        //     }
         
-            throw $e;
-        }
+        //     throw $e;
+        // }
     }   
 }
